f5aa684d6620c5f2f4656dfff44e58f515efe698
Krupali1994 <krupalicharlie2728@gmail.com>
2023-10-19 14:29:49 +0100

    javaweek1writinghw

---
+++ src/JavaWeek1WritingHW/Programme_4_RightTriangle.java
@@ -0,0 +1,22 @@
+package JavaWeek1WritingHW;
+
+/**
+ * 4. Write a Java program to print below triangle
+ * <p>
+ * *
+ * **
+ * ***
+ * ****
+ * *****
+ * ******
+ */
+public class Programme_4_RightTriangle {
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
+        System.out.println("******");
+    }
+}
